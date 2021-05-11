f8903a39f14db8b2594aa8a4fd0404cb8befd9d2
BadreddineZatout <hb_zatout@esi.dz>
2021-05-11 20:22:30 +0100

    deleted extra files

--- app/models/index.js
+++ app/models/index.js
@@ -19,7 +19,6 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-// db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
 db.locataires = require("./locataire.model.js")(sequelize, Sequelize);
 
 module.exports = db;
