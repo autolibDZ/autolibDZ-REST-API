0b12e230758c5d099a67b8aae53b43b88820510f
SamDz16 <samdz161999@gmail.com>
2021-05-12 18:12:47 +0100

    handling errors

--- app/controllers/abonnement.controller.js
+++ app/controllers/abonnement.controller.js
@@ -1,47 +1,41 @@
-const db = require("../models");
+const db = require('../models');
 const Abonnement = db.abonnement;
 
-// get the balance 
+// get the balance
 const getUserBalance = async (req, res) => {
-    
-    // Validate request
-    if (!req.params.id) {
-      res.status(400).send({
-        message: "params 'id' can not be empty!"
-      });
-      return;
-    }
-    
-    
-    // read the balance from DB
-    try{
-        const id = req.params.id
-        const balance = await Abonnement.findAll({
-                                where: {
-                                    idLocataire: id
-                                },
-                                attributes : ['balance']
-                        });
-        console.log(balance)
-        
-        if( balance.length != 0){
-            res.send(balance);
-        }else{
-            res.status(404).send({
-                "error" : "the id " + id + " does not exist"
-            })
-        }
-        
+	// Validate request
+	if (!req.params.id) {
+		res.status(400).send({
+			message: "params 'id' can not be empty!",
+		});
+		return;
+	}
 
-    }
-    catch(err){
-        res.status(500).send({
-            error : err.message || "Some error occurred !"
-        });
-    }
-     
-  };
+	// read the balance from DB
+	try {
+		const id = req.params.id;
+		const balance = await Abonnement.findAll({
+			where: {
+				idLocataire: id,
+			},
+			attributes: ['balance'],
+		});
+		console.log(balance);
 
-  export default {
-    getUserBalance
-  }
+		if (balance.length != 0) {
+			res.send(balance);
+		} else {
+			res.status(404).send({
+				error: 'the id ' + id + ' does not exist',
+			});
+		}
+	} catch (err) {
+		res.status(500).send({
+			error: err.message || 'Some error occurred !',
+		});
+	}
+};
+
+export default {
+	getUserBalance,
+};
