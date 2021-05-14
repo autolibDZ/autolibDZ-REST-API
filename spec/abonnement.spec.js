480b36b5a69d450c8d483903da269c653007cff6
DokkarRachidReda <hr_dokkar@esi.dz>
2021-05-14 19:29:11 +0100

    added unit tests for getBalance

--- spec/abonnement.spec.js
+++ spec/abonnement.spec.js
@@ -1,8 +1,6 @@
 const Request = require('supertest');
 request = Request('http://localhost:4000/api/abonnement');
 
-
-
 describe('Abonnement route test', () => {
 
   describe('getBalance 1st scenario', () => {
@@ -14,13 +12,44 @@ describe('Abonnement route test', () => {
         .expect('Content-Type', 'application/json; charset=utf-8')
         .end((err, res) => {
             if (err) done(err);
-
-            //expect(res.body.balance != null)
+            
+            expect(res.body.balance)
             
             done();
           });
       
     });
+
+
+    it('returns 404 Not found when using an non exesting id 13', (done) => {
+        request
+          .get('/13')
+          .expect(404)
+          .expect('Content-Type', 'application/json; charset=utf-8')
+          .end((err, res) => {
+              if (err) done(err);
+              
+              expect(res.body.error == 'the id 13 does not exist')
+              
+              done();
+            });
+        
+      });
+
+      it('returns 500  server error when using a wrong id like 1m', (done) => {
+        request
+          .get('/1m')
+          .expect(500)
+          .expect('Content-Type', 'application/json; charset=utf-8')
+          .end((err, res) => {
+              if (err) done(err);
+              
+              expect(res.body.error)
+              
+              done();
+            });
+        
+      });
   });
 
 });
