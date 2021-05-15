7d4ed3bfa645d4de06addb06a9cb1bb12b7fb940
sana <hs_boucheta@esi.dz>
2021-05-15 14:57:32 +0100

    handling errors in test

--- spec/transaction.spec.js
+++ spec/transaction.spec.js
@@ -1,7 +1,43 @@
 var request = require("request");
 var base_url = "http://localhost:4000"
 
+// Test "createTransaction" 
+describe("add transaction", function () {
+     describe("post/", function () {
+          it("add one transaction", function (done) {
+               request.post({
+                    url: "http://localhost:4000/api/transaction", form: {
+                         "idLocataire": 1,
+                         "montant": 222.2,
+                         "moyenPayement": "Paypal",
+                         "idReservation": 3
+                    }
+               }, function (error, response, body) {
+                    console.log('error:', error); // Print the error if one occurred
+                    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                    expect('Content-Type', 'application/json; charset=utf-8')
+                    done();
+               });
+          });
 
+          it("deplicat reservation id", function (done) {
+               request.post({
+                    url: "http://localhost:4000/api/transaction", form: {
+                         "idLocataire": 1,
+                         "montant": 222.2,
+                         "moyenPayement": "Paypal",
+                         "idReservation": 3
+                    }
+               }, function (error, response, body) {
+                    console.log('error:', error); // Print the error if one occurred
+                    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                    console.log('body:', body);
+                    expect(response.statusCode).toBe(500);
+                    done();
+               });
+          });
+     });
+})
 
 //Test "getUserTransactions" 
 
@@ -13,7 +49,7 @@ describe("Test get transaction", function () {
                  expect(JSON.parse(body)).toEqual([
                     {
                         idTransaction: 9,
-                        dateTransaction: "2021-05-15T13:17:43.736Z",
+                        dateTransaction: "2021-05-15T13:26:06.426Z",
                         montant: 222.2,
                         moyenPayement: "Paypal",
                         idLocataire: 1,
