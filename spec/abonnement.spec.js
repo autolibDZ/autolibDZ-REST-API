5eab5ebe30229bd47e2f3bde79376ec4b59c1038
DokkarRachidReda <hr_dokkar@esi.dz>
2021-05-15 14:13:25 +0100

    added toBe to the expectations to evaluate em (must be added and it was missing) and added doPayment tests

--- spec/abonnement.spec.js
+++ spec/abonnement.spec.js
@@ -3,7 +3,7 @@ request = Request('http://localhost:4000/api/abonnement');
 
 describe('Abonnement route test', () => {
 
-  describe('getBalance 1st scenario', () => {
+  describe('GET getBalance : returns the user balance', () => {
     
     it('returns 200 OK when using an exesting id 1', (done) => {
       request
@@ -13,7 +13,7 @@ describe('Abonnement route test', () => {
         .end((err, res) => {
             if (err) done(err);
             
-            expect(res.body.balance)
+            expect(res.body.balance != null).toBe(true)
             
             done();
           });
@@ -29,7 +29,7 @@ describe('Abonnement route test', () => {
           .end((err, res) => {
               if (err) done(err);
               
-              expect(res.body.error == 'the id 13 does not exist')
+              expect(res.body.error == 'the id 13 does not exist').toBe(true)
               
               done();
             });
@@ -44,7 +44,67 @@ describe('Abonnement route test', () => {
           .end((err, res) => {
               if (err) done(err);
               
-              expect(res.body.error)
+              expect(res.body.error != null).toBe(true)
+              
+              done();
+            });
+        
+      });
+  });
+
+
+
+  describe('POST doPayment : update the user balance', () => {
+    
+    it('returns 200 OK when using an exesting id 1', (done) => {
+      request
+        .post('/1')
+        .send({
+          prix : 0.5 // 0.5 so that the balance won't reach 0 and the test fails
+        })
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .end((err, res) => {
+            if (err) done(err);
+
+            expect(res.body.message == "payment done").toBe(true)
+            
+            done();
+          });
+      
+    });
+
+
+    it('returns 404 Not found when using an non exesting id 13', (done) => {
+        request
+          .post('/13')
+          .send({
+            prix : 0.5 // 0.5 so that the balance won't reach 0 and the test fails
+          })
+          .expect(404)
+          .expect('Content-Type', 'application/json; charset=utf-8')
+          .end((err, res) => {
+              if (err) done(err);
+              
+              expect(res.body.error == 'the id 13 does not exist').toBe(true)
+              
+              done();
+            });
+        
+      });
+
+      it('returns 500  server error when using a wrong id like 1m', (done) => {
+        request
+          .post('/1m')
+          .send({
+            prix : 0.5 // 0.5 so that the balance won't reach 0 and the test fails
+          })
+          .expect(500)
+          .expect('Content-Type', 'application/json; charset=utf-8')
+          .end((err, res) => {
+              if (err) done(err);
+              
+              expect(res.body.error != null).toBe(false)
               
               done();
             });
