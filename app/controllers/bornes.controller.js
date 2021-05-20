7c36ffe7994518f0b20a96f8d8b12cbcca0098b8
DokkarRachidReda <hr_dokkar@esi.dz>
2021-05-20 23:00:21 +0100

    added getFilteredBornes to the controller

--- app/controllers/bornes.controller.js
+++ app/controllers/bornes.controller.js
@@ -1,5 +1,6 @@
 const db = require("../models");
 const Borne = db.borne;
+const { Op } = require("sequelize");
 /**
  * Create and save a new borne in database
  * @param {*} req The request
@@ -65,36 +66,100 @@ const createBorne = async (req, res) => {
  * Return a list of borne according to parameter filter in body request
  * @param {*} req The request
  * @param {*} res The response
+ * body elements : if nothing is provided it matches all
+ * wilaya 
+ * nomBorne
+ * commune
+ * nbVehicules
+ * nbVehiculesOp : > , < = the default is >
+ * nbPlaces
+ * nbPlacesOp : > , < = the default is >
  */
 
 //Returne list of Bornes
 
 const getFilteredBornes = async (req, res) => {
 
+  if (!req.body) {
+		res.status(400).send({
+			message: "body can not be empty!",
+		});
+		return;
+	}
+
+  const ops = ['<' , '>' , '=']
+
+  if (req.body.nbPlacesOp != null && ! ops.includes(req.body.nbPlacesOp)) {
+		res.status(400).send({
+			message: "nbPlacesOp must be > , < or = ",
+		});
+		return;
+	}
+
+  if (req.body.nbVehiculesOp != null && ! ops.includes(req.body.nbVehiculesOp)) {
+		res.status(400).send({
+			message: "nbVehiculesOp must be > , < or = ",
+		});
+		return;
+	}
+
   try {
+    
+    // setting the operator < , > , =
+    const nbVehiculesOperator = (req.body.nbVehiculesOp != null) ? req.body.nbVehiculesOp :  '>'
+    const nbPlacesOperator = (req.body.nbPlacesOp != null) ? req.body.nbPlacesOp :  '>'
 
-    // Get all bornes in database
+    // setting squelize Op
 
-    if (req.body.filter == "") {
+    var nbPlacesSquelizeOp;
 
-      const data = await Borne.findAll()
-
-      console.log(data)
-
-      if (data != null && data.length != 0) {
-
-        res.send(data);
-
-      } else {
-
-        res.status(404).send({
-
-          message: "Borne table is empty!"
-
-        })
-      }
-
+    if(nbPlacesOperator == '>'){
+      nbPlacesSquelizeOp = Op.gt
+    }else if(nbPlacesOperator == '<'){
+      nbPlacesSquelizeOp = Op.lt
+    }else{
+      nbPlacesSquelizeOp = Op.eq
     }
+
+    var nbVehiculesSquelizeOp ;
+
+    if(nbVehiculesOperator == '>'){
+      nbVehiculesSquelizeOp = Op.gt
+    }else if(nbVehiculesOperator == '<'){
+      nbVehiculesSquelizeOp = Op.lt
+    }else{
+      nbVehiculesSquelizeOp = Op.eq
+    }
+
+    const bornes = await Borne.findAll({
+			where: {
+				nomBorne: {
+          [Op.like] : (req.body.nomBorne != null) ? req.body.nomBorne :  '%'
+        },
+        wilaya: {
+          [Op.like] : (req.body.wilaya != null) ? req.body.wilaya :  '%'
+        },
+        commune: {
+          [Op.like] : (req.body.commune != null) ? req.body.commune :  '%'
+        },
+        nbVehicules: {
+          [nbVehiculesSquelizeOp] : (req.body.nbVehicules != null) ? req.body.nbVehicules :  0
+        },
+        nbPlaces: {
+          [nbPlacesSquelizeOp] : (req.body.nbPlaces != null) ? req.body.nbPlaces :  0
+        }
+			},
+			
+		});
+
+     if (bornes.length != 0) {
+	        res.send(bornes);
+	    } else {
+	        res.status(404).send({
+	            error: 'there is no Born that matches your filter',
+	        });
+	    }
+    
   }
   catch (err) {
 
