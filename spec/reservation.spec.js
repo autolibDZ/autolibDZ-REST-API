d4cf4eb37b7ae83ea87c181c1a9b802a04c9b916
ayabendjeddou <47477960+ayabendjeddou@users.noreply.github.com>
2021-05-22 00:37:57 +0100

    Update

--- spec/reservation.spec.js
+++ spec/reservation.spec.js
@@ -1,4 +1,9 @@
+
 const Request = require('supertest');
+
+
+/*const Request = require('supertest');
+
 request = Request('http://localhost:4000/api/reservation');
 
 describe('add reservation', function () {
@@ -26,3 +31,27 @@ describe('add reservation', function () {
 		});
 	});
 });
+
+
+*/
+const Request = require('supertest');
+request = Request('http://localhost:4000/api');
+
+describe('Testing GET on /api/reservation endpoint', () => {
+    it('should return the list of all reservations stored in the database, at least one reservation', (done) => {
+        request
+            .get('/reservation')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    done.fail(err);
+                } else {
+                    expect(res.body.length).not.toEqual(0);
+                    done();
+                }
+            });
+    });
+});
+
