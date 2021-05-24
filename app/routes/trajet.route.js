451da10251e523ae95c650866535b09196cdf49a
Diaeddin BOUIDAINE <56446721+dbouidaine@users.noreply.github.com>
2021-05-24 13:30:39 +0100

    Update trajet.route.js

--- app/routes/trajet.route.js
+++ app/routes/trajet.route.js
@@ -3,13 +3,13 @@ import trajetController from "../controllers/trajet.controller";
 
 var trajetRouter = require("express").Router();
 
+trajetRouter.get("/countByMonth/:year", trajetController.countTrajetsByMonth);
+trajetRouter.get("/getYears", trajetController.getYears);
 trajetRouter.post('/', trajetController.createTrajet);
 trajetRouter.get( '/', trajetController.listAllTrajets);
 trajetRouter.get('/:id', trajetController.findTrajetById);
 trajetRouter.put('/:id', trajetController.updateTrajetById);
 trajetRouter.delete('/:id', trajetController.deleteTrajetById);
-trajetRouter.get("/countByMonth/:year", trajetController.countTrajetsByMonth);
-trajetRouter.get("/getYears", trajetController.getYears);
 
 export default trajetRouter;
 
