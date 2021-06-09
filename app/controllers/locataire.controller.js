65a41e26dd6df023fd43bee9b229245b96383fd1
Badreddine Zatout <57811383+BadreddineZatout@users.noreply.github.com>
2021-06-09 23:14:11 +0100

    Update locataire.controller.js

--- app/controllers/locataire.controller.js
+++ app/controllers/locataire.controller.js
@@ -216,7 +216,7 @@ const updateEmail = async (req, res) => {
 	
 	//Pour tester l'existance de l'email
 	const locataires = await Locataire.findOne({
-		where: { email: req.body.email },
+		where: { email: req.body.email, isDelete: false },
 	});
 	const locataire = await Locataire.findOne({
 		where: {
