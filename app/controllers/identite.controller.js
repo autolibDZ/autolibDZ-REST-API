092dd26b7d9c362c9fcbf04d54bec02dce14a053
Doumi Athmane <ha_doumi@esi.dz>
2021-07-10 20:47:43 +0200

    delete double identite

--- app/controllers/identite.controller.js
+++ app/controllers/identite.controller.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 	cloud_name: process.env.CLOUD_NAME,
 	api_key: process.env.API_KEY,
 	api_secret: process.env.API_SECRET,
-});*/ 
+});*/
 
 
 /**
@@ -19,196 +19,203 @@ require('dotenv').config();
  * @param {*} req la requete, on doit mettre dans body :- photo(pour photo du permis) -selfie(pour selfie du locataire) et idLocataire
  * @param {*} res la reponse
  */
-const createIdentite = async (req, res) => {
+const createIdentite = async(req, res) => {
+
+    const idnt = await Identite.findOne({ where: { idLocataire: req.body.idLocataire } })
+    if (idnt != null) {
+        await Identite.destroy({
+            where: { idLocataire: req.body.idLocataire }
+        })
+
+    }
+
     // Create an identite
     const identite = {
         idLocataire: req.body.idLocataire,
         idCloudinary: req.body.idPhoto,
-        secureUrl:req.body.photo,
+        secureUrl: req.body.photo,
         idCloudinaryPhotoSelfie: req.body.idSelfie,
-        secureUrlPhotoSelfie:req.body.selfie
+        secureUrlPhotoSelfie: req.body.selfie
 
     };
 
     // Ajout d'une identité à la base de données
-    if(identite.idLocataire==undefined /* || identite.idOperateur==undefined*/){
-      res.status(500).send({
-          message:"Le champ idLocataire est requis!"             
-      });
-      return;
-    }
-    else{
-      try{
-        // upload image to cloudinary here
-        /*if (req.body.photo) {
-            const image = req.body.photo;
-            try {
-              ress = await cloudinary.uploader.upload(req.body.photo).then((result) => {
-                identite.idCloudinary = result.public_id;
-                identite.secureUrl = result.secure_url;
-                console.log(result)
-            });
-            } catch (error) {
-                console.log(error);
-              }
-            }else{
-              res.status(500).send({
-                message:"Vous devez entrez une image!"             
-            });
-            return;
-            }
-
-           if (req.body.selfie) {
-              const image = req.body.photo;
-              try {
-                ress = await cloudinary.uploader.upload(req.body.photo).then((result) => {
-                  identite.idCloudinaryPhotoSelfie = result.public_id;
-                  identite.secureUrlPhotoSelfie = result.secure_url;
-                  console.log(result)
-              });
-              } catch (error) {
-                  console.log(error);
+    if (identite.idLocataire == undefined /* || identite.idOperateur==undefined*/ ) {
+        res.status(500).send({
+            message: "Le champ idLocataire est requis!"
+        });
+        return;
+    } else {
+        try {
+            // upload image to cloudinary here
+            /*if (req.body.photo) {
+                const image = req.body.photo;
+                try {
+                  ress = await cloudinary.uploader.upload(req.body.photo).then((result) => {
+                    identite.idCloudinary = result.public_id;
+                    identite.secureUrl = result.secure_url;
+                    console.log(result)
+                });
+                } catch (error) {
+                    console.log(error);
+                  }
+                }else{
+                  res.status(500).send({
+                    message:"Vous devez entrez une image!"             
+                });
+                return;
                 }
-              }else{
-                res.status(500).send({
-                  message:"Vous devez entrez une image de votre visage dans la borne!"             
-              });
-              return;
-              }*/ 
-        data = await Identite.create(identite)
-       .then(data => {
-        res.status(200).send(data);
-       }); 
-      }
-      catch(err){
-          res.status(500).send({
-              error : err.message || "Some error occurred while creating the Identity."
-          });
-      }
+
+               if (req.body.selfie) {
+                  const image = req.body.photo;
+                  try {
+                    ress = await cloudinary.uploader.upload(req.body.photo).then((result) => {
+                      identite.idCloudinaryPhotoSelfie = result.public_id;
+                      identite.secureUrlPhotoSelfie = result.secure_url;
+                      console.log(result)
+                  });
+                  } catch (error) {
+                      console.log(error);
+                    }
+                  }else{
+                    res.status(500).send({
+                      message:"Vous devez entrez une image de votre visage dans la borne!"             
+                  });
+                  return;
+                  }*/
+            data = await Identite.create(identite)
+                .then(data => {
+                    res.status(200).send(data);
+                })
+        } catch (err) {
+            res.status(500).send({
+                error: err.message || "Some error occurred while creating the Identity."
+            });
+        }
     }
-     
-  };
+
+};
 
 /**
  * Supprimer une identité
  * @param {*} req la requete
  * @param {*} res la reponse
  */
-  const deleteIdentite = async (req, res) => {
-      const id = req.params.id;
-     
+const deleteIdentite = async(req, res) => {
+    const id = req.params.id;
 
-      Identite.destroy({
-        where: { id: id }
-      })
+
+    Identite.destroy({
+            where: { id: id }
+        })
         .then(num => {
-          if (num == 1) {
-            res.status(200).send({
-              message: "Identity was deleted successfully!"
-            });
-          } else {
-            res.send({
-              message: `Cannot delete Identity with licence driver=${id}. Maybe identity was not found!`
-            });
-          }
+            if (num == 1) {
+                res.status(200).send({
+                    message: "Identity was deleted successfully!"
+                });
+            } else {
+                res.send({
+                    message: `Cannot delete Identity with licence driver=${id}. Maybe identity was not found!`
+                });
+            }
         })
         .catch(err => {
-          res.status(500).send({
-            message: "Could not delete Tutorial with id=" + id
-          });
+            res.status(500).send({
+                message: "Could not delete Tutorial with id=" + id
+            });
         });
-    };
+};
 
 /**
  * valider une identité en rendant le champ valide à 1
  * @param {*} req la requete
  * @param {*} res la reponse
  */
-  const valider= async (req, res) => {
+const valider = async(req, res) => {
     const id = req.params.id;
-  
+
     req.body = {
         valide: 1
     }
 
     Identite.update(req.body, {
-      where: { id: id }
-    })
-      .then(num => {
-        if (num == 1) {
-          res.status(200).send({
-            message: "identity was validated successfully."
-          });
-        } else {
-          res.send({
-            message: `Cannot validate identity with numPermis=${id}. Maybe Identity was not found!`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error validating identity with numPermis=" + id
+            where: { id: id }
+        })
+        .then(num => {
+            if (num == 1) {
+                res.status(200).send({
+                    message: "identity was validated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot validate identity with numPermis=${id}. Maybe Identity was not found!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error validating identity with numPermis=" + id
+            });
         });
-      });
-  };
+};
 /**
  * Invalider une identité en rendant le champ valide à 0
  * @param {*} req la requete
  * @param {*} res la reponse
  */
-  const invalider= async (req, res) => {
+const invalider = async(req, res) => {
     const id = req.params.id;
-  
+
     req.body = {
         valide: 0
     }
 
     Identite.update(req.body, {
-      where: { id: id }
-    })
-      .then(num => {
-        if (num == 1) {
-          res.status(200).send({
-            message:  "identity was invalidated successfully."
-          });
-        } else {
-          res.send({
-            message: `Cannot invalidate identity with numPermis=${id}. Maybe Identity was not found!`
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error invalidating identity with numPermis=" + id
+            where: { id: id }
+        })
+        .then(num => {
+            if (num == 1) {
+                res.status(200).send({
+                    message: "identity was invalidated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot invalidate identity with numPermis=${id}. Maybe Identity was not found!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error invalidating identity with numPermis=" + id
+            });
         });
-      });
-  };
+};
 /**
  * Afficher les informations de l'opérateur ayant validé l'identité
  * @param {*} req la requete
  * @param {*} res la reponse
  */
-const getOperatorOfIdentity = async (req, res) =>{
-  //Récupérer le numéro de permis validé
-  const numeroPermis = req.params.numeroPermis;
-  Identite.findByPk(numeroPermis)
-  .then(data => {
-    const idOperateur = data.dataValues.idOperateur;
-    Operator.findByPk(idOperateur)
-    .then(data => {
-      res.status(200).send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Erreur lors de la récupération de l'Operateur ayant validé le permis =" + numeroPermis
-      });
-    });
-  })
-  .catch(err => {
-    res.status(500).send({
-      message: "Erreur lors de la récupération de l'Operateur ayant validé le permis =" + numeroPermis
-    });
-  });
+const getOperatorOfIdentity = async(req, res) => {
+    //Récupérer le numéro de permis validé
+    const numeroPermis = req.params.numeroPermis;
+    Identite.findByPk(numeroPermis)
+        .then(data => {
+            const idOperateur = data.dataValues.idOperateur;
+            Operator.findByPk(idOperateur)
+                .then(data => {
+                    res.status(200).send(data);
+                })
+                .catch(err => {
+                    res.status(500).send({
+                        message: "Erreur lors de la récupération de l'Operateur ayant validé le permis =" + numeroPermis
+                    });
+                });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Erreur lors de la récupération de l'Operateur ayant validé le permis =" + numeroPermis
+            });
+        });
 }
 
 
@@ -218,28 +225,28 @@ const getOperatorOfIdentity = async (req, res) =>{
  * @param {*} res la reponse
  */
 
-const getLocataireOfIdentity = async (req, res) =>{
-  //Récupérer le numéro de permis validé
-  const id = req.params.id;
-  Identite.findByPk(id)
-  .then(data => {
-    const idLocataire = data.dataValues.idLocataire;
-    console.log(idLocataire);
-    Locataire.findByPk(idLocataire)
-    .then(data => {
-       res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Erreur lors de la récupération du locataire pour le permis=" + id
-      });
-    });
-  })
-  .catch(err => {
-    res.status(500).send({
-      message: "Erreur lors de la récupération de l'Operateur ayant validé le permis =" + id
-    });
-  });
+const getLocataireOfIdentity = async(req, res) => {
+    //Récupérer le numéro de permis validé
+    const id = req.params.id;
+    Identite.findByPk(id)
+        .then(data => {
+            const idLocataire = data.dataValues.idLocataire;
+            console.log(idLocataire);
+            Locataire.findByPk(idLocataire)
+                .then(data => {
+                    res.send(data);
+                })
+                .catch(err => {
+                    res.status(500).send({
+                        message: "Erreur lors de la récupération du locataire pour le permis=" + id
+                    });
+                });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Erreur lors de la récupération de l'Operateur ayant validé le permis =" + id
+            });
+        });
 }
 
 
@@ -251,18 +258,18 @@ const getLocataireOfIdentity = async (req, res) =>{
  * @param {*} req la requete
  * @param {*} res la reponse
  */
-const getOneIdentite= async (req, res) => {
-  const id = req.params.id;
-  
-  Identite.findByPk(id)
-  .then(data => {
-    res.status(200).send(data);
-  })
-  .catch(err => {
-    res.status(500).send({
-      message: "Error retrieving Identity with id=" + id
-    });
-  });
+const getOneIdentite = async(req, res) => {
+    const id = req.params.id;
+
+    Identite.findByPk(id)
+        .then(data => {
+            res.status(200).send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving Identity with id=" + id
+            });
+        });
 };
 
 
@@ -272,17 +279,17 @@ const getOneIdentite= async (req, res) => {
  * @param {*} req la requete
  * @param {*} res la reponse
  */
-const getAllIdentite = async (req,res)=> {
-    
-  Identite.findAll()
-  .then(data => {
-    res.send(data);
-  })
-  .catch(err => {
-    res.status(500).send({
-        message: "Error while retrieving identities"
-    });
-  });
+const getAllIdentite = async(req, res) => {
+
+    Identite.findAll()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error while retrieving identities"
+            });
+        });
 }
 
 
@@ -291,28 +298,27 @@ const getAllIdentite = async (req,res)=> {
  * @param {*} req la requete
  * @param {*} res la reponse
  */
- const selectIdentitieOfAGivenLocataire = async (req, res) => {
-	try {
-		const identities = await Identite.findOne({
-			where: {
-				idLocataire: +req.params.id,
-			},
-		});
-		res.status(200).send(identities);
-	} catch (err) {
-		res.status(500).send({
-			error:
-				err.message ||
-				'Some error occured while retreiving identity of locataire id: ' +
-					req.params.id,
-		});
-	}
+const selectIdentitieOfAGivenLocataire = async(req, res) => {
+    try {
+        const identities = await Identite.findOne({
+            where: {
+                idLocataire: +req.params.id,
+            },
+        });
+        res.status(200).send(identities);
+    } catch (err) {
+        res.status(500).send({
+            error: err.message ||
+                'Some error occured while retreiving identity of locataire id: ' +
+                req.params.id,
+        });
+    }
 };
 
 
 export default {
     //selectIdentitiesOfAGivenOperateur,
-    createIdentite, 
+    createIdentite,
     deleteIdentite,
     getAllIdentite,
     getOneIdentite,
